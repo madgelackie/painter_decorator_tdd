4f2b1134a5af55b73436e68090b852f02e8e88c6
madgelackie <marionmlackie@gmail.com>
2021-10-13 09:24:53 +0100

    room completed

--- specs/models_specs.js
+++ specs/models_specs.js
@@ -5,21 +5,28 @@ const Decorator = require('../models.js');
 
 describe('Room', function(){
 
-    // beforeEach(function (){
-    //     room = new Room(20, False);
-    // })
-
-});
+    beforeEach(function (){
+        room = new Room(20, false);
+    })
 
     it('should have an area', function (){
-        const room = new Room(20);
+        // const room = new Room(20);
         const actual = room.area;
         assert.strictEqual(actual, 20)
     })
 
-    xit('should start not painted')
+    it('should start not painted', function (){
+        // const room = new Room (20, false);
+        const actual = room.isPainted;
+        assert.strictEqual(actual, false)
+    })
 
-    xit('should be able to be painted')
+    it('should be able to be painted', function (){
+        // const room = new Room (20, false);
+        const actual = room.hasBeenPainted();
+        assert.strictEqual(actual, true)
+    })
+});
 
 xdescribe('Paint_Can', function(){
 
